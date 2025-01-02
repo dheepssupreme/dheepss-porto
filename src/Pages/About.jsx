103f8b199ae34ce6cfabdb1e0e30cf4d7e72782d
dheepssupreme <dheepss312@gmail.com>
2025-01-02 15:51:54 +0700

    fix

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, memo, useMemo } from "react"
 import { FileText, Code, Award, Globe, ArrowUpRight, Sparkles, UserCheck } from "lucide-react"
 import AOS from 'aos'
 import 'aos/dist/aos.css'
+import fotoImage from "../public/foto.jpg";
 
 // Memoized Components
 const Header = memo(() => (
@@ -50,7 +51,7 @@ const ProfileImage = memo(() => (
           <div className="absolute inset-0 bg-gradient-to-t from-purple-500/20 via-transparent to-blue-500/20 z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-700 hidden sm:block" />
           
           <img
-            src="public/foto.jpg"
+            image:fotoImage
             alt="Profile"
             className="w-full h-full object-cover transition-all duration-700 group-hover:scale-110 group-hover:rotate-2"
             loading="lazy"
